c5eedcda029aabc5552fa197299455022464f63d
I324578 <karthikeyan.s02@sap.com>
2022-01-28 11:36:13 +0530

    Add params

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -23,6 +23,25 @@ def call(body) {
     body()
 
     node{
+
+        //Implemention of params
+
+        def jobParameters = [
+                    string(defaultValue: "", description: 'Product Path', name: 'PRODUCT_PATH', trim: false),
+                    string(defaultValue: '', description: 'Build Number', name: 'BUILD_NUMBER', trim: false),
+                    string(defaultValue: '', description: 'Enter an existing HOA JIRA issue (leave it empty to automatically create a new one)', name: 'HOA_TICKET', trim: true),
+                    string(defaultValue: "", description: 'Comma Separated Prod Deploy Modes', name: 'PROD_DEPLOY_MODES', trim: false)
+            ]
+            properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '90', numToKeepStr: '20')), disableConcurrentBuilds(),
+                        parameters(jobParameters), pipelineTriggers(config.periodicTrigger)])
+            def productPath     = params.PRODUCT_PATH
+            def buildNumberEx   = params.BUILD_NUMBER
+            currentBuild.displayName = buildNumberEx
+            def buildPath   = "workarea/${productPath}/${buildNumberEx}"
+            
+
+        //Ends here    
+
         stage('New closures'){
             println('name is ' +config.Name)
             println('Value is ' +config.Value)
